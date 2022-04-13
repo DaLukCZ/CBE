2c5220e6651b5148711977f1b6d2e767ee365915
DaLuk <lukas.smach@seznam.cz>
2022-04-13 14:33:15 +0200

    fsf

---
+++ Ekonomikare/Database.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Chcete_byt_EXPERTEM
+{
+    partial class Database
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Database));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.checkBox3 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.checkBox2 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.checkBox4 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.Obor = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.odpoved1 = new System.Windows.Forms.TextBox();
+            this.odpoved2 = new System.Windows.Forms.TextBox();
+            this.odpoved3 = new System.Windows.Forms.TextBox();
+            this.odpoved4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1094, 929);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(798, 100);
+            this.button2.TabIndex = 53;
+            this.button2.Text = "Zpět";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 929);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(798, 100);
+            this.button3.TabIndex = 54;
+            this.button3.Text = "Uložit";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.checkBox1.CheckedState.BorderRadius = 2;
+            this.checkBox1.CheckedState.BorderThickness = 0;
+            this.checkBox1.CheckedState.FillColor = System.Drawing.Color.White;
+            this.checkBox1.CheckMarkColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(940, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 75);
+            this.checkBox1.TabIndex = 55;
+            this.checkBox1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.checkBox1.UncheckedState.BorderRadius = 0;
+            this.checkBox1.UncheckedState.BorderThickness = 0;
+            this.checkBox1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.checkBox3.CheckedState.BorderRadius = 2;
+            this.checkBox3.CheckedState.BorderThickness = 0;
+            this.checkBox3.CheckedState.FillColor = System.Drawing.Color.White;
+            this.checkBox3.CheckMarkColor = System.Drawing.Color.Black;
+            this.checkBox3.Location = new System.Drawing.Point(940, 500);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(75, 75);
+            this.checkBox3.TabIndex = 56;
+            this.checkBox3.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.checkBox3.UncheckedState.BorderRadius = 0;
+            this.checkBox3.UncheckedState.BorderThickness = 0;
+            this.checkBox3.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.checkBox2.CheckedState.BorderRadius = 2;
+            this.checkBox2.CheckedState.BorderThickness = 0;
+            this.checkBox2.CheckedState.FillColor = System.Drawing.Color.White;
+            this.checkBox2.CheckMarkColor = System.Drawing.Color.Black;
+            this.checkBox2.Location = new System.Drawing.Point(940, 375);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(75, 75);
+            this.checkBox2.TabIndex = 57;
+            this.checkBox2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.checkBox2.UncheckedState.BorderRadius = 0;
+            this.checkBox2.UncheckedState.BorderThickness = 0;
+            this.checkBox2.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.CheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.checkBox4.CheckedState.BorderRadius = 2;
+            this.checkBox4.CheckedState.BorderThickness = 0;
+            this.checkBox4.CheckedState.FillColor = System.Drawing.Color.White;
+            this.checkBox4.CheckMarkColor = System.Drawing.Color.Black;
+            this.checkBox4.Location = new System.Drawing.Point(940, 625);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(75, 75);
+            this.checkBox4.TabIndex = 58;
+            this.checkBox4.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.checkBox4.UncheckedState.BorderRadius = 0;
+            this.checkBox4.UncheckedState.BorderThickness = 0;
+            this.checkBox4.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            // 
+            // Obor
+            // 
+            this.Obor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.Obor.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.Obor.ForeColor = System.Drawing.Color.White;
+            this.Obor.Location = new System.Drawing.Point(65, 781);
+            this.Obor.Multiline = true;
+            this.Obor.Name = "Obor";
+            this.Obor.Size = new System.Drawing.Size(815, 75);
+            this.Obor.TabIndex = 59;
+            this.Obor.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(65, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(950, 150);
+            this.textBox1.TabIndex = 60;
+            this.textBox1.Visible = false;
+            // 
+            // odpoved1
+            // 
+            this.odpoved1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.odpoved1.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.odpoved1.ForeColor = System.Drawing.Color.White;
+            this.odpoved1.Location = new System.Drawing.Point(65, 250);
+            this.odpoved1.Multiline = true;
+            this.odpoved1.Name = "odpoved1";
+            this.odpoved1.Size = new System.Drawing.Size(815, 75);
+            this.odpoved1.TabIndex = 61;
+            this.odpoved1.Visible = false;
+            // 
+            // odpoved2
+            // 
+            this.odpoved2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.odpoved2.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.odpoved2.ForeColor = System.Drawing.Color.White;
+            this.odpoved2.Location = new System.Drawing.Point(65, 375);
+            this.odpoved2.Multiline = true;
+            this.odpoved2.Name = "odpoved2";
+            this.odpoved2.Size = new System.Drawing.Size(815, 75);
+            this.odpoved2.TabIndex = 62;
+            this.odpoved2.Visible = false;
+            // 
+            // odpoved3
+            // 
+            this.odpoved3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.odpoved3.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.odpoved3.ForeColor = System.Drawing.Color.White;
+            this.odpoved3.Location = new System.Drawing.Point(65, 500);
+            this.odpoved3.Multiline = true;
+            this.odpoved3.Name = "odpoved3";
+            this.odpoved3.Size = new System.Drawing.Size(815, 75);
+            this.odpoved3.TabIndex = 63;
+            this.odpoved3.Visible = false;
+            // 
+            // odpoved4
+            // 
+            this.odpoved4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
+            this.odpoved4.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Bold);
+            this.odpoved4.ForeColor = System.Drawing.Color.White;
+            this.odpoved4.Location = new System.Drawing.Point(65, 625);
+            this.odpoved4.Multiline = true;
+            this.odpoved4.Name = "odpoved4";
+            this.odpoved4.Size = new System.Drawing.Size(815, 75);
+            this.odpoved4.TabIndex = 64;
+            this.odpoved4.Visible = false;
+            // 
+            // Database
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Chcete_byt_EXPERTEM.Properties.Resources.ask;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.odpoved4);
+            this.Controls.Add(this.odpoved3);
+            this.Controls.Add(this.odpoved2);
+            this.Controls.Add(this.odpoved1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Obor);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Database";
+            this.Text = "Database";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox checkBox1;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox checkBox3;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox checkBox2;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox checkBox4;
+        private System.Windows.Forms.TextBox Obor;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox odpoved1;
+        private System.Windows.Forms.TextBox odpoved2;
+        private System.Windows.Forms.TextBox odpoved3;
+        private System.Windows.Forms.TextBox odpoved4;
+    }
+}
